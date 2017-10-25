b311b7db457d1fdd5302d37068e7b0533df0cd4c
annette-bessant <33102868+annette-bessant@users.noreply.github.com>
2017-10-25 18:04:18 -0400

    initial commit

---
+++ views/site/index.php
@@ -0,0 +1,31 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Menu';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-menu">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Please fill out the following fields to login:</p>
+    <div class="menu" >
+
+            <!--Selection BAR -->
+            <div class="col-md-3">
+                <p class="lead">Create Page</p>
+                <div class="list-group">
+                    <a class="list-group-item"href=menu.php>Menus</a>
+                    <a class="list-group-item">Pages</a>
+                    <a class="list-group-item">Pictures</a>
+                </div>
+           </div>
+         </div>
+
+
+</div>
